4923d60e192b65473bbd04ca090bd0e36a6b5600
Linden Jouni <jouni.linden@gmail.com>
2017-10-28 10:46:50 +0300

    package to hackdaycache

--- cache_test.go
+++ cache_test.go
@@ -1,31 +1,13 @@
-package main
+package hackdaycache
 
 import (
-	"io/ioutil"
-	"log"
-	"net"
-	"net/http"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/jelinden/hackdaycache/client"
 	"github.com/stretchr/testify/assert"
 )
-
-var client *http.Client
-
-func init() {
-	client = &http.Client{
-		Transport: &http.Transport{
-			MaxIdleConnsPerHost: 64,
-			Dial: (&net.Dialer{
-				Timeout:   5 * time.Second,
-				KeepAlive: 10 * time.Second,
-			}).Dial,
-			ResponseHeaderTimeout: 5 * time.Second,
-		},
-	}
-}
 
 func TestSetItem(t *testing.T) {
 	url := "https://httpbin.org/ip"
@@ -34,7 +16,7 @@ func TestSetItem(t *testing.T) {
 		Value:        nil,
 		Expire:       time.Now(),
 		UpdateLength: 1 * time.Second,
-		GetFunc:      dataFetch,
+		GetFunc:      client.DataFetch,
 		InUse:        false,
 	}
 	AddItem(i)
@@ -43,24 +25,4 @@ func TestSetItem(t *testing.T) {
 	cache.Remove(url)
 	assert.False(t, strings.Contains(string(GetItem(url)), "origin"))
 	assert.True(t, string(GetItem(url)) == "")
-}
-
-func httpGet(url string) ([]byte, error) {
-	t := time.Now()
-	resp, err := client.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	log.Println("GET", resp.StatusCode, url, time.Now().Sub(t), time.Now().Format("15:04:05.000"))
-	return body, nil
-}
-
-func dataFetch(url string) []byte {
-	data, err := httpGet(url)
-	if err != nil {
-		log.Println("getting", url, "failed", err.Error())
-	}
-	return data
 }
